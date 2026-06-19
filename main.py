257d7e2bd5de6ae672e3590eacca6dffcb9a16dd
Rafael Rodriguez <rafaelr@familiarodriguez.net>
2026-06-19 17:15:51 -0400

    Step 5 - add "read" as a parameter to __init__ and default to False.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 class Email:
-    def __init__(self, sender, receiver, subject, body):
+    def __init__(self, sender, receiver, subject, body, read=False):
         self.sender = sender
         self.receiver = receiver
         self.subject = subject
         self.body = body
+        self.read = read
 
 email_obj = Email("alice@example.com","bob@example.com","Hello","Hi Bob!")
 
 print(email_obj.sender)
 print(email_obj.subject)
+print(email_obj.read)
