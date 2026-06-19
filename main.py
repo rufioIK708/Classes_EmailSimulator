65355df3c907e3d0053aeb94029d99b88d1c21cb
Rafael Rodriguez <rafaelr@familiarodriguez.net>
2026-06-19 17:24:28 -0400

    Step 9 - remove email_obj and all print statements

--- main.py
+++ main.py
@@ -9,10 +9,3 @@ class Email:
     def mark_as_read(self):
         self.read = True
         
-email_obj = Email("alice@example.com","bob@example.com","Hello","Hi Bob!")
-
-print(email_obj.sender)
-print(email_obj.subject)
-print(email_obj.read)
-email_obj.mark_as_read()
-print(email_obj.read)
